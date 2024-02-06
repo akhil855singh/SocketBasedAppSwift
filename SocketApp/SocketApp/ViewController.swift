2caba6e8944bb3f86af0082cab113bc8269741e5
Akhil <158463748+akhilrepcard@users.noreply.github.com>
2024-02-06 14:03:34 +0530

    first commit

---
+++ SocketApp/SocketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketApp
+//
+//  Created by Akhil on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
